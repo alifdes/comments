5f1c67287e591b226a633ea6de6723fed2c9d2c3
DESKTOP-CCT2684\user <alifdes1123@gmail.com>
2023-04-07 02:38:23 +0530

    added github url

--- src/services/comments.js
+++ src/services/comments.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getComments = async () => {
-    const response = await axios.get('data.json');
+    const response = await axios.get('https://raw.githubusercontent.com/alifdes/comments/main/public/data.json');
     return response.data;
   };
   
